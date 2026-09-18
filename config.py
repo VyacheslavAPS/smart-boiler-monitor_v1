d7189d407f1cbfb35c96d35cf433238b25e984db
VyacheslavAPS <aps2007.vo@gmail.com>
2026-09-18 17:54:56 +0300

    Clear API_URL, TG_TOKEN, and TG_CHAT_ID
    Removed sensitive information from config.py.

--- config.py
+++ config.py
@@ -3,12 +3,13 @@
 WIFI_SSID = "Your_Actual_SSID"
 WIFI_PASS = "Your_Actual_Password"
 
-API_URL = "http://192.168.1.100"  # IP FastAPI сервера
+API_URL =  # IP FastAPI сервера
 
-TG_TOKEN = "123456789:ABCdefGHIjklMNOpqrsTUVwxyZ"
-TG_CHAT_ID = "987654321"
+TG_TOKEN = ""
+TG_CHAT_ID = ""
 
 # Порты и пины при необходимости тоже можно вынести сюда
+
 DS_PIN = 4
 GRID_PIN = 14
 INVERTER_PIN = 12
